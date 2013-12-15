e7e3ff780d3a8aef48250023e2d18058d0c0a608
karton33 <vladimir.filarsky@gmail.com>
2013-12-15 23:04:39 +0100

    zobrazovac zoznamu uloh podla dni

--- views/tasks/view.php
+++ views/tasks/view.php
@@ -1,5 +1,31 @@
 <?
-
 foreach ($this->tasks as $task):
-    echo $task->getName() . '<br>';
+    $class = '';
+    $priority = $task->getPriority();
+    if ($priority > 0 && $priority <= 3) {
+        $class = 'prio' . $priority;
+    }
+    $parentTask = '';
+    if ($task->getParentTask() instanceof Figeor\Models\Task) {
+        $parentTask = ' / ' . $task->getParentTask()->getName();
+    }
+    ?>
+    <div class="uloha <?= $class; ?>">
+
+        <div>
+            <span class="icon icon-date-task"></span><?= $task->getName(); ?>
+            (<span class="icon icon-project"></span><?= $task->getProject()->getName() . $parentTask . ' '; ?>)
+            <span class="icon icon-time"></span><?= $task->getDeadline('d.m.Y H:i:s'); ?>
+        </div>
+
+        <span class="icon icon-chart-down"></span><?= $task->getPoints(); ?> bodov
+
+        <span class="icon icon-comment"></span><?= $task->getDescription(); ?></p>
+    <div style="clear: both;"></div>
+    </div>
+    <?
 endforeach;
+
+if (!sizeof($this->tasks)):
+    echo 'V zadanom rozsahu sa nenachádzajú žiadne úlohy.';
+endif;
